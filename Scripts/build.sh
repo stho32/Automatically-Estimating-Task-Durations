51ca6412bfe77eedde42e7bec210fc67c15bcd90
Stefan Hoffmann <stho32github@gmail.com>
2021-09-11 06:25:28 +0200

    modifying to use latex as primary input

---
+++ Scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is the build script for the project. It creates graphs and measurements from 
+# the 2 datasets that I have available at the moment freshly everytime I run it.
+# It helps to standardize all the images and other calculations. And helps with the
+# division between the data that I am allowed to share and the rest.
+# It is not necessary to create the pdf from the files contained within this repository.
+# So if you are just looking to create the pdf or change some text you can do that
+# without executing this.
+
