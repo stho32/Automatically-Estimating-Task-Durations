57fcb3a7cc115e88cb942dc9979fadac84271c91
Stefan Hoffmann <stho32github@gmail.com>
2021-09-12 14:55:09 +0200

    summary for graphs is automatically generated now

---
+++ Scripts/create_latex_for_graphs.py
@@ -0,0 +1,21 @@
+import glob
+import json
+import tabulate
+
+def load_json_data(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        print(data)
+        return data
+
+files = glob.glob("./output/*.png") 
+files = sorted(files)
+
+result = ""
+
+for f in files:
+    f = f.replace("./output", "Scripts/output")
+    result += "%\\includegraphics[width=\\textwidth]{" + f + "}\n"
+
+with open("./output/summary-graphs.tex", "w") as summaryfile:
+    summaryfile.write(result)
