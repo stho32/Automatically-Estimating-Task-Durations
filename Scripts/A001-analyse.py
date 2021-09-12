b081c6edf5d97a15870bf5186fd5e951f7070ef1
Stefan Hoffmann <stho32github@gmail.com>
2021-09-12 08:17:57 +0200

    images update

--- Scripts/A001-analyse.py
+++ Scripts/A001-analyse.py
@@ -6,10 +6,11 @@ import plotly.express as px
 import numpy
 
 def convert_to_hours(columnName, row):
+    if row[columnName] == 0:
+        return 0.0
     numberAsString = row[columnName].replace(",", ".")
     number = float(numberAsString)
     hours = number / 60 / 60
-    #print("convert", numberAsString, "to", number, "=", hours, "h")
     return hours
 
 def create_image(filename):
@@ -19,16 +20,25 @@ def create_image(filename):
     df = df.sort_values('HoursSpent')
     df['Task'] = range(1,len(df)+1)
 
-    fig = px.scatter(df, y="HoursSpent", x="Task")
+    fig = go.Figure() 
+
+    fig.add_scatter(y=df["HoursSpent"], x=df["Task"], name="Hours spent")
     fig.add_scatter(x=df["Task"], y=df["HoursEstimated"], mode="markers", name="Hours estimated")
 
     fig.update_layout(
-        title='Hours spent per task and hours estimated (2020)',
+        title = filename,
         plot_bgcolor='rgb(230, 230,230)',
         showlegend=True)
 
+    fig.update_layout(
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ))
+
     fig.update_traces(marker_size=5)
-    #fig.update_xaxes(visible=False, showticklabels=False)
 
     fig.write_image(filename + ".png", width=1200, height=700, scale=1)
     fig.show()
@@ -38,4 +48,15 @@ def analyse(filename):
 
 
 analyse('./output/A001_swearchiv2020_swearchiv2020_25.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2020_50.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2020_75.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2020_80.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2020_90.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2020_100.csv');
 
+analyse('./output/A001_swearchiv2020_swearchiv2021_25.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2021_50.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2021_75.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2021_80.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2021_90.csv');
+analyse('./output/A001_swearchiv2020_swearchiv2021_100.csv');
