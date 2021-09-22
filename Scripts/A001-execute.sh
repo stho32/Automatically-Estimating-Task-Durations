e8b1d24b0ebecabd43fcb87dc83e904a6b663aaf
Stefan Hoffmann <stho32github@gmail.com>
2021-09-22 04:01:26 +0200

    a001 now is implemented in python

--- Scripts/A001-execute.sh
+++ Scripts/A001-execute.sh
@@ -6,7 +6,6 @@ python3 ../Source/a001/a001.py --learn --input ../../Automatically-Estimating-Ta
 
 #python3 ../Source/a003_1/a003_1.py --estimate --input "Ticketsystem: neuer Antrag für Ingo" --model ./a003_1_model.json
 
-#python3 ../Source/a003_1/a003_1.py --validation --input ../../Automatically-Estimating-Task-Durations-Private/SWE-Archiv-2020.csv --output ./output/A003_1_swearchiv2020_swearchiv2020.csv --model ./a003_1_model.json
+python3 ../Source/a001/a001.py --validation --input ../../Automatically-Estimating-Task-Durations-Private/SWE-Archiv-2020.csv --output ./output/A001_swearchiv2020_swearchiv2020_025.csv --model ./a001_model.json --probabilityInPercent 25
 
-#python3 ../Source/a003_1/a003_1.py --validation --input ../../Automatically-Estimating-Task-Durations-Private/SWE-Archiv-2021.csv --output ./output/A003_1_swearchiv2020_swearchiv2021.csv --model ./a003_1_model.json
 
