37b33c80cd28ff22b3e2beee9b31f0614244fdb9
Stefan Hoffmann <stho32github@gmail.com>
2021-09-25 04:59:38 +0200

    execution can now be controlled more effectivly

--- Scripts/execute.py
+++ Scripts/execute.py
@@ -3,6 +3,7 @@
     This script executes all algorithms against their configured data combinations
     using the configured parameters.
 """
+from genericpath import exists
 import analytics.grab_data
 import analytics.exec_combinations as ec
 import os
@@ -10,13 +11,34 @@ import pandas as pd
 import analytics.high_level as hl
 
 def analyse(algorithm, trainingOn, estimating, parameter, filename):
-    print("   ANALYSE - " + filename + "...")
+    """
+        Execute analysis
+    """
+    print("   analysing the result - " + filename + "...")
     
     df = analytics.grab_data.grab_prepared_data(filename)
 
     hl.create_image_scatterHours(filename, df)
     hl.create_image_boxplot(filename, df)
     hl.create_statistics(algorithm, trainingOn, estimating, parameter, filename, df)
+
+def execute_combination(algorithm_definition, commandline):
+    """
+        Execute combination
+        Should there be an output generated, then we start the analysis
+    """
+    if not os.system(commandline["command"]) == 0:
+        print("There has been a problem. Stopping execution.")
+        exit()
+
+    if not commandline["output"] == None:
+        analyse(
+            algorithm_definition["name"], 
+            commandline["trainOn"], 
+            commandline["estimate"], 
+            commandline["parameter"],
+            commandline["output"]
+        )
 
 os.system('cls' if os.name=='nt' else 'clear')
 
@@ -43,15 +65,17 @@ for algorithm_definition in configuration["algorithms"]:
             commandline = combination
             print("    - " + algorithm_definition["name"] + ": " + commandline["command"])
 
-            if not os.system(commandline["command"]) == 0:
-                print("There has been a problem. Stopping execution.")
-                exit()
-
+            # If an output is defined we check if there is a file like that 
+            # already on the disk. If it is we do not need to repeat the execution
+            # and analysis - unless it is explicitly requested.
+            # This way we save time when developing new algorithms.
             if not commandline["output"] == None:
-                analyse(
-                    algorithm_definition["name"], 
-                    commandline["trainOn"], 
-                    commandline["estimate"], 
-                    commandline["parameter"],
-                    commandline["output"]
-                )
+                if (os.path.exists(commandline["output"])):
+                    # ... and the repeat is not requested
+                    if algorithm_definition["force_run"]:
+                        execute_combination(algorithm_definition, commandline)
+                    else:
+                        print("      -> execution not needed, output already there")
+                else:
+                    execute_combination(algorithm_definition, commandline)
+
